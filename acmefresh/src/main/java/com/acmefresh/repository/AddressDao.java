50b04dd86374f7b394d10c93f547f83ebfb99c43
Rounak Abraham <rounakabraham@gmail.com>
2022-07-21 00:20:55 +0530

    Delete acmefresh directory

--- acmefresh/src/main/java/com/acmefresh/repository/AddressDao.java
+++
@@ -1,9 +0,0 @@
-package com.acmefresh.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.acmefresh.model.Address;
-
-public interface AddressDao extends JpaRepository<Address, Integer> {
-
-}
